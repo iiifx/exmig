79f223a1004e7e5e6377b87aad264aaed11e8f74
Vitaliy IIIFX Khomenko <iiifx@yandex.ru>
2015-01-09 18:10:22 +0200

    Запрет на использование сидера

--- source/Seeder.php
+++ source/Seeder.php
@@ -3,8 +3,16 @@
 namespace ExMig;
 
 use Yii;
+use yii\base\ErrorException;
 
 class Seeder extends migration\BaseMigration {
+
+    public function init () {
+        /*
+         * Пока не использовать, до реализации
+         */
+        throw new ErrorException( 'Is not implemented' );
+    }
 
     /*
      * В данный момент функционал не реализован
