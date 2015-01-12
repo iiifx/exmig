f91b3839a66fe695e83fa141a7edb92fb80ae5a5
Vitaliy IIIFX Khomenko <iiifx@yandex.ru>
2015-01-12 12:25:36 +0200

    Исправления

--- examples/m141007_072426_CREATE_user_profile.php
+++ examples/m141007_072426_CREATE_user_profile.php
@@ -5,12 +5,12 @@ class m141007_072426_CREATE_user_profile extends \Exmig\Migration {
     public $tableName = 'user_profile';
 
     public function safeUp () {
-        $this->createEntityTable( array (
-            'user_id'      => "INT(11) NULL DEFAULT NULL",
-            'first_name'   => "VARCHAR(128) NOT NULL DEFAULT ''",
-            'last_name'    => "VARCHAR(128) NOT NULL DEFAULT ''",
-            'date_birth'   => "DATE NOT NULL",
-        ) );
+        $this->createEntityTable( [
+            'user_id'    => "INT(11) NULL DEFAULT NULL",
+            'first_name' => "VARCHAR(128) NOT NULL DEFAULT ''",
+            'last_name'  => "VARCHAR(128) NOT NULL DEFAULT ''",
+            'date_birth' => "DATE NOT NULL",
+        ] );
         $this->createForeignKey( 'user', 'user_id' );
     }
 
